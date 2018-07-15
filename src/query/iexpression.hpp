e06bcc10a70b79c27c75ae4da9b5cf2c0a373854
kele <damian.bogel@gmail.com>
2018-07-15 16:34:16 +0200

    removed comments

--- src/query/iexpression.hpp
+++ src/query/iexpression.hpp
@@ -11,8 +11,6 @@ class Environment;
 class IExpression {
 public:
   virtual Value eval(std::shared_ptr<const Environment> env) const = 0;
-  // TODO: implement in subclasses
-  // virtual std::type_info typecheck(const Environment &env) const = 0;
 
   virtual ~IExpression() {}
 };
